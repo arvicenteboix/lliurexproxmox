b244120fbb2b98e678873431c49b8991c6ef3e35
alviboi <alviboi@gmail.com>
2024-05-07 21:23:28 +0200

    adaptacio2024

--- Tema 2: Model de centre amb virtualbox/crea.sh
+++ Tema 2: Model de centre amb virtualbox/crea.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pandoc Model_de_centre_virtualbox.md -o Model_de_centre_virtualbox.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
+pandoc M1.md -o M1.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
+pandoc M1_es.md -o M1_es.pdf --from markdown+implicit_figures --template eisvogel --listings --filter pandoc-latex-environment --number-sections --toc
+
--- Tema 5: Configuració Proxmox/crea_html.sh
+++ Tema 5: Configuració Proxmox/crea_html.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pandoc Configuracio_proxmox.md -o index_va.html --from markdown+implicit_figures -c pandoc.css --template elegant_bootstrap_menu --listings --filter pandoc-latex-environment --number-sections --toc --toc-depth 3
+pandoc M1.md -o index.html --from markdown+implicit_figures -c aqua.css --template ./plantilla.html --listings --filter pandoc-latex-environment --number-sections --toc --toc-depth 3
+pandoc M1_es.md -o index_es.html --from markdown+implicit_figures -c aqua.css --template ./plantilla.html --listings --filter pandoc-latex-environment --number-sections --toc --toc-depth 3
